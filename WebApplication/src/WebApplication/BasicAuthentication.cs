61d77c7df93efeaa01dcc690f6f5c754acb2fbc5
James Malherbe <james.malherbe@gmail.com>
2017-01-24 06:09:09 +0200

    Poly Application

---
+++ WebApplication/src/WebApplication/BasicAuthentication.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class BasicAuthentication
+    {
+        private readonly RequestDelegate _next;
+
+        public BasicAuthentication(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Path.ToString().StartsWith("/api/"))
+            {
+                await _next.Invoke(context);
+                return;
+            }
+
+            string authHeader = context.Request.Headers["Authorization"];
+            if (authHeader == null || !authHeader.StartsWith("Basic"))
+            {
+                context.Response.StatusCode = 401; //Unauthorized
+                return;
+            }
+
+            string encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+            Encoding encoding = Encoding.GetEncoding("iso-8859-1");
+            string usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+            int seperatorIndex = usernamePassword.IndexOf(':');
+
+            var username = usernamePassword.Substring(0, seperatorIndex);
+            var password = usernamePassword.Substring(seperatorIndex + 1);
+
+            if (username == "username1" && password == "password2")
+                await _next.Invoke(context);
+            else
+                context.Response.StatusCode = 403;
+        }
+    }
+}
---
+++ WebApplication/src/WebApplication/Model/Persoon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class Persoon
+    {
+        private static int _id = 0;
+
+        public Persoon()
+        {
+            Id = ++_id;
+        }
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public string Van { get; set; }
+    }
+}
