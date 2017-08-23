c76242302526fc3645dc547cc6e098fa20b7bcb8
James Malherbe <james.malherbe@gmail.com>
2017-08-23 20:37:38 +0200

    UISWeb Model Changes

---
+++ UISWeb/UISWeb/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UISWeb.Models
+{
+    public class Customer
+    {
+        [Key, StringLength(255)]
+        public string Email { get; set; }
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [StringLength(255)]
+        public string CompanyName { get; set; }
+        //[Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ClientId { get; set; }
+
+        public virtual ICollection<CustomerFile> Reports { get; set; }
+    }
+}
---
+++ UISWeb/UISWeb/Models/CustomerFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UISWeb.Models
+{
+    public class CustomerFile
+    {
+        [Key, StringLength(255)]
+        public string FileName { get; set; }
+        [StringLength(255)]
+        public string LinkName { get; set; }
+    }
+}
